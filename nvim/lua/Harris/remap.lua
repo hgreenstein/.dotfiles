f9768e065e3c7d5c6135844e7292955560de3ca6
Harris Greenstein <harrisgreenstein@gmail.com>
2023-12-17 23:02:16 -0500

    Adding Ctrl+Motion for switching buffer windows quickly
    Additionally added remapping comments in remap.lua

--- nvim/lua/Harris/remap.lua
+++ nvim/lua/Harris/remap.lua
@@ -2,10 +2,18 @@
 --vim.keymap.set("n", "<leader>pv", api.tree.toggle())
 vim.keymap.set({"n", "v"}, "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>P", [["+p]])
+--Yanks to system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+--Auto centers window for better scrolling
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+vim.keymap.set({"n", "i", "v"}, "<C-l>", "<C-w>l")
+vim.keymap.set({"n", "i", "v"}, "<C-k>", "<C-w>k")
+vim.keymap.set({"n", "i", "v"}, "<C-j>", "<C-w>j")
+vim.keymap.set({"n", "i", "v"}, "<C-h>", "<C-w>h")
+--Pulls up LazyGit in Neovim
 vim.keymap.set("n", "<leader>gg", ':LazyGit<CR>')
 vim.g.clipboard = {
   name = 'WslClipboard',
