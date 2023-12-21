e89baf7a5bc455fa89c8051a9161cfd9244a1915
Harris Greenstein <harrisgreenstein@gmail.com>
2023-12-20 22:07:29 -0500

    Adding Back LuaSnip
    This plugin keeps giving issues but is heavily required for some lsp functionality,
    I will continue troubleshooting this issue

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -32,6 +32,13 @@ return {
             'neovim/nvim-lspconfig',
             'hrsh7th/nvim-cmp',
             'hrsh7th/cmp-nvim-lsp',
+            {
+                "L3MON4D3/LuaSnip",
+                -- follow latest release.
+                version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+                -- install jsregexp (optional!).
+                build = "make install_jsregexp"
+            },
         }
     },
 
