5a4e28b937012d1a76d3243af82776610645cdd7
Harris Greenstein <harrisgreenstein@gmail.com>
2023-12-26 22:00:09 -0500

    Tmux & Nvim: Adding Vim Tmux Navigator
    Allows seemless window switching between both neovim buffers and tmux panes with ctrl + hjkl

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -125,4 +125,22 @@ return {
             "nvim-lua/plenary.nvim",
         },
     },
+    -- Tmux Navigation with ctrl + hjkl
+    {
+        "christoomey/vim-tmux-navigator",
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+        },
+        keys = {
+            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
+    },
 }
