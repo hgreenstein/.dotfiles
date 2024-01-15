8c54c4ca4889bfe2413a2c9291f07a6c682ed26f
Harris Greenstein <harrisgreenstein@gmail.com>
2024-01-14 21:05:37 -0500

    Neovim: Added Noice.nvim
    Adds prettier notification messages
    https://github.com/folke/noice.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -159,23 +159,38 @@ return {
     'mxsdev/nvim-dap-vscode-js',
     'jay-babu/mason-nvim-dap.nvim',
     {
-    "David-Kunz/gen.nvim",
-    opts = {
-        model = "codellama:7b", -- The default model to use.
-        display_mode = "float", -- The display mode. Can be "float" or "split".
-        show_prompt = true, -- Shows the Prompt submitted to Ollama.
-        show_model = true, -- Displays which model you are using at the beginning of your chat session.
-        no_auto_close = true, -- Never closes the window automatically.
-        init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
-        -- Function to initialize Ollama
-        command = "curl --silent --no-buffer -X POST http://localhost:11434/api/generate -d $body",
-        -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
-        -- This can also be a lua function returning a command string, with options as the input parameter.
-        -- The executed command must return a JSON object with { response, context }
-        -- (context property is optional).
-        list_models = '<omitted lua function>', -- Retrieves a list of model names
-        debug = false -- Prints errors and the command which is run.
-    }
-},
+        "David-Kunz/gen.nvim",
+        opts = {
+            model = "codellama:7b", -- The default model to use.
+            display_mode = "float", -- The display mode. Can be "float" or "split".
+            show_prompt = true, -- Shows the Prompt submitted to Ollama.
+            show_model = true,  -- Displays which model you are using at the beginning of your chat session.
+            no_auto_close = true, -- Never closes the window automatically.
+            init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
+            -- Function to initialize Ollama
+            command = "curl --silent --no-buffer -X POST http://localhost:11434/api/generate -d $body",
+            -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+            -- This can also be a lua function returning a command string, with options as the input parameter.
+            -- The executed command must return a JSON object with { response, context }
+            -- (context property is optional).
+            list_models = '<omitted lua function>', -- Retrieves a list of model names
+            debug = false                       -- Prints errors and the command which is run.
+        }
+    },
     { 'folke/which-key.nvim', opts = {} },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    },
 }
